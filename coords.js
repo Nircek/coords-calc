4ea0fce653fa032e7aea7b09c508f2f95077c9a0
Marcin Zepp <nircek-2103@protonmail.com>
2024-03-22 19:29:25 +0200

    Add Maidenhead grid

--- coords.js
+++ coords.js
@@ -1,5 +1,6 @@
 export const DD_RE = /^([-+]?(?:[1-8]?\d(?:\.\d+)?|90(?:\.0+)?)), ([-+]?(?:180(?:\.0+)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))(?:\.\d+)?))$/;
 export const DMS_RE = /^(?:[1-8]?\d°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?|90°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?)?) ?[NS] (?:180°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?)?) ?[EW]$/
+export const MAIDENHEAD_RE = /^[A-R]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2})?)?)?)?)?)?)?$/;
 
 export function tidy_dd(dd_str) {
     return dd_str
@@ -15,6 +16,10 @@ export function tidy_dms(dms_str) {
         .replace(/'/g, "′")
         .replace(/′′|"/g, "″")
         .replace(/[^0-9.°′″NESW ]/, "");
+}
+
+export function tidy_maidenhead(maidenhead_str) {
+    return maidenhead_str.substr(0, 2) + maidenhead_str.substr(2).toLowerCase();
 }
 
 export function convert_dd(str) {
@@ -38,6 +43,29 @@ export function convert_dms(str) {
     return [(sign_lat ? -1 : 1) * lat, (sign_long ? -1 : 1) * long];
 }
 
+const charCode_A = 'A'.charCodeAt(0);
+const charCode_a = 'a'.charCodeAt(0);
+
+export function convert_maidenhead(str) {
+    let long = -180, lat = -90;
+    long += 20 * (str.charCodeAt(0) - charCode_A);
+    lat += 10 * (str.charCodeAt(1) - charCode_A);
+    let dlong = 20, dlat = 10;
+    for (let i = 1; 2 * i < str.length; ++i) {
+        if (i % 2 == 1) {
+            dlong /= 10; dlat /= 10;
+            long += str[2 * i] * dlong;
+            lat += str[2 * i + 1] * dlat;
+        } else {
+            dlong /= 24; dlat /= 24;
+            long += (str.charCodeAt(2 * i) - charCode_a) * dlong;
+            lat += (str.charCodeAt(2 * i + 1) - charCode_a) * dlat;
+        }
+    }
+
+    return [lat + dlat / 2, long + dlong / 2];
+}
+
 export function generate_dd(lat, long) {
     return `${lat.toFixed(7)}, ${long.toFixed(7)}`;
 }
@@ -52,4 +80,29 @@ function generate_dms_str(dd, long) {
 
 export function generate_dms(lat, long) {
     return `${generate_dms_str(lat, false)} ${generate_dms_str(long, true)}`;
+}
+
+const maidenhead_alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWX';
+const maidenhead_alphabet_lower = maidenhead_alphabet_upper.toLowerCase();
+
+export function generate_maidenhead(lat, long) {
+
+    if (long >= 180) long = -180;
+
+    long += 180; lat += 90;
+    let square = [long / 20, lat / 10].map(x => maidenhead_alphabet_upper[x | 0]).join('');
+    long = long % 20 / 20; lat = lat % 10 / 10;
+    for (let i = 1; i < 5; ++i) {
+        console.log(i, long, lat);
+        if (i % 2 == 1) {
+            square += `${long * 10 | 0}${lat * 10 | 0}`
+            long = long * 10 % 1;
+            lat = lat * 10 % 1;
+        } else {
+            square += [long * 24, lat * 24].map(x => maidenhead_alphabet_lower[x | 0]).join('');
+            long = long * 24 % 1;
+            lat = lat * 24 % 1;
+        }
+    }
+    return square;
 }
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import {
     DD_RE,
     DMS_RE,
+    MAIDENHEAD_RE,
     tidy_dd,
     tidy_dms,
+    tidy_maidenhead,
     convert_dd,
     convert_dms,
+    convert_maidenhead,
     generate_dd,
-    generate_dms
+    generate_dms,
+    generate_maidenhead
 } from "./coords.js";
 
 var lat, long, last;
@@ -19,34 +23,26 @@ function updateValue(el, value) {
 function updateAll() {
     updateValue(document.querySelector('#dd'), generate_dd(lat, long));
     updateValue(document.querySelector('#dms'), generate_dms(lat, long));
+    updateValue(document.querySelector('#maidenhead'), generate_maidenhead(lat, long));
 }
 
-document.querySelector('#dd').addEventListener('change', (ev) => {
-    ev.target.value = tidy_dd(ev.target.value);
-});
+function addInput(id, tidy, re, convert) {
+    document.querySelector(id).addEventListener('change', (ev) => {
+        ev.target.value = tidy(ev.target.value);
+    });
 
-document.querySelector('#dd').addEventListener('input', (ev) => {
-    const value = tidy_dd(ev.target.value);
-    const valid = DD_RE.test(value);
-    if (!valid) { ev.target.classList.add("invalid"); return; }
-    ev.target.classList.remove("invalid");
+    document.querySelector(id).addEventListener('input', (ev) => {
+        const value = tidy(ev.target.value);
+        const valid = re.test(value);
+        if (!valid) { ev.target.classList.add("invalid"); return; }
+        ev.target.classList.remove("invalid");
 
-    [lat, long] = convert_dd(value);
-    last = ev.target;
-    updateAll();
-});
+        [lat, long] = convert(value);
+        last = ev.target;
+        updateAll();
+    });
+}
 
-document.querySelector('#dms').addEventListener('change', (ev) => {
-    ev.target.value = tidy_dms(ev.target.value);
-});
-
-document.querySelector('#dms').addEventListener('input', (ev) => {
-    const value = tidy_dms(ev.target.value);
-    const valid = DMS_RE.test(value);
-    if (!valid) { ev.target.classList.add("invalid"); return; }
-    ev.target.classList.remove("invalid");
-
-    [lat, long] = convert_dms(value);
-    last = ev.target;
-    updateAll();
-});
+addInput('#dd', tidy_dd, DD_RE, convert_dd);
+addInput('#dms', tidy_dms, DMS_RE, convert_dms);
+addInput('#maidenhead', tidy_maidenhead, MAIDENHEAD_RE, convert_maidenhead);
