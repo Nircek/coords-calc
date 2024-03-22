f99069e11937ff04470ac0cc4e48112b04a19e05
Marcin Zepp <nircek-2103@protonmail.com>
2024-03-22 19:54:42 +0200

    Make it more object

--- coords.js
+++ coords.js
@@ -1,108 +1,111 @@
-export const DD_RE = /^([-+]?(?:[1-8]?\d(?:\.\d+)?|90(?:\.0+)?)), ([-+]?(?:180(?:\.0+)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))(?:\.\d+)?))$/;
-export const DMS_RE = /^(?:[1-8]?\d°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?|90°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?)?) ?[NS] (?:180°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?)?) ?[EW]$/
-export const MAIDENHEAD_RE = /^[A-R]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2})?)?)?)?)?)?)?$/;
+export class DecimalDegrees {
+    static regex = /^([-+]?(?:[1-8]?\d(?:\.\d+)?|90(?:\.0+)?)), ([-+]?(?:180(?:\.0+)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))(?:\.\d+)?))$/;
+    static tidy(str) {
+        return str
+            .replace(/,/g, ", ")
+            .replace(/\s{2,}/g, " ")
+            .replace(/[^-+0-9,. ]/, "");
 
-export function tidy_dd(dd_str) {
-    return dd_str
-        .replace(/,/g, ", ")
-        .replace(/\s{2,}/g, " ")
-        .replace(/[^-+0-9,. ]/, "");
-}
-
-export function tidy_dms(dms_str) {
-    return dms_str
-        .replace(/\s{2,}/g, " ")
-        .replace(/d/g, "°")
-        .replace(/'/g, "′")
-        .replace(/′′|"/g, "″")
-        .replace(/[^0-9.°′″NESW ]/, "");
-}
-
-export function tidy_maidenhead(maidenhead_str) {
-    return maidenhead_str.substr(0, 2) + maidenhead_str.substr(2).toLowerCase();
-}
-
-export function convert_dd(str) {
-    const [_, lat, long] = str.match(DD_RE);
-    return [+lat, +long];
-}
-
-function convert_dms_str(str) {
-    const [deg, min, sec] = [...str.split(/[°′″]/g), 0, 0];
-    console.log(deg, min, sec);
-    return + deg + min / 60 + sec / 3600;
-}
-
-export function convert_dms(str) {
-    const [str_lat, str_long] = str.replace(" ", "").split(/[NESW]/g);
-    const [sign_lat, sign_long] = [...str.replace(/[^NESW]/g, "")]
-        .map((e) => e == "S" || e == "W");
-
-    const [lat, long] = [str_lat, str_long].map(convert_dms_str);
-    console.log(lat, long);
-    return [(sign_lat ? -1 : 1) * lat, (sign_long ? -1 : 1) * long];
-}
-
-const charCode_A = 'A'.charCodeAt(0);
-const charCode_a = 'a'.charCodeAt(0);
-
-export function convert_maidenhead(str) {
-    let long = -180, lat = -90;
-    long += 20 * (str.charCodeAt(0) - charCode_A);
-    lat += 10 * (str.charCodeAt(1) - charCode_A);
-    let dlong = 20, dlat = 10;
-    for (let i = 1; 2 * i < str.length; ++i) {
-        if (i % 2 == 1) {
-            dlong /= 10; dlat /= 10;
-            long += str[2 * i] * dlong;
-            lat += str[2 * i + 1] * dlat;
-        } else {
-            dlong /= 24; dlat /= 24;
-            long += (str.charCodeAt(2 * i) - charCode_a) * dlong;
-            lat += (str.charCodeAt(2 * i + 1) - charCode_a) * dlat;
-        }
     }
+    static convert(str) {
+        const [_, lat, long] = str.match(DecimalDegrees.regex);
+        return [+lat, +long];
 
-    return [lat + dlat / 2, long + dlong / 2];
-}
-
-export function generate_dd(lat, long) {
-    return `${lat.toFixed(7)}, ${long.toFixed(7)}`;
-}
-
-function generate_dms_str(dd, long) {
-    const sign = dd < 0;
-    if (sign) dd = -dd;
-    dd = Math.round(dd * 3600 * 1000 | 0) / 1000;
-    const dir = "NESW"[sign * 2 + long];
-    return `${dd / 3600 | 0}° ${dd % 3600 / 60 | 0}′ ${(dd % 60).toFixed(3)}″ ${dir}`;
-}
-
-export function generate_dms(lat, long) {
-    return `${generate_dms_str(lat, false)} ${generate_dms_str(long, true)}`;
-}
-
-const maidenhead_alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWX';
-const maidenhead_alphabet_lower = maidenhead_alphabet_upper.toLowerCase();
-
-export function generate_maidenhead(lat, long) {
-
-    if (long >= 180) long = -180;
-
-    long += 180; lat += 90;
-    let square = [long / 20, lat / 10].map(x => maidenhead_alphabet_upper[x | 0]).join('');
-    long = long % 20 / 20; lat = lat % 10 / 10;
-    for (let i = 1; i < 5; ++i) {
-        console.log(i, long, lat);
-        if (i % 2 == 1) {
-            square += `${long * 10 | 0}${lat * 10 | 0}`
-            long = long * 10 % 1;
-            lat = lat * 10 % 1;
-        } else {
-            square += [long * 24, lat * 24].map(x => maidenhead_alphabet_lower[x | 0]).join('');
-            long = long * 24 % 1;
-            lat = lat * 24 % 1;
-        }
     }
-    return square;
+    static generate(lat, long) {
+        return `${lat.toFixed(7)}, ${long.toFixed(7)}`;
+    }
+}
+
+export class DegreesMinutesSeconds {
+    static regex = /^(?:[1-8]?\d°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?|90°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?)?) ?[NS] (?:180°(?: ?00?′(?: ?00?(?:.00?0?)?″)?)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))°(?: ?\d\d?′(?: ?\d\d?(?:.\d\d?\d?)?″)?)?) ?[EW]$/;
+    static tidy(str) {
+        return str
+            .replace(/d/g, "°")
+            .replace(/'/g, "′")
+            .replace(/′′|"/g, "″")
+            .toUpperCase()
+            .replace(/[^0-9.°′″NESW ]/, "")
+            .replace(/°/g, "° ")
+            .replace(/′/g, "′ ")
+            .replace(/″/g, "″ ")
+            .replace(/\s{2,}/g, " ");
+    }
+    static #convert_dms_str(str) {
+        const [deg, min, sec] = [...str.split(/[°′″]/g), 0, 0];
+        return + deg + min / 60 + sec / 3600;
+    }
+    static convert(str) {
+        const [str_lat, str_long] = str.replace(" ", "").split(/[NESW]/g);
+        const [sign_lat, sign_long] = [...str.replace(/[^NESW]/g, "")]
+            .map((e) => e == "S" || e == "W");
+
+        const [lat, long] = [str_lat, str_long].map(DegreesMinutesSeconds.#convert_dms_str);
+        return [(sign_lat ? -1 : 1) * lat, (sign_long ? -1 : 1) * long];
+    }
+    static #generate_dms_str(dd, long) {
+        const sign = dd < 0;
+        if (sign) dd = -dd;
+        dd = Math.round(dd * 3600 * 1000 | 0) / 1000;
+        const dir = "NESW"[sign * 2 + long];
+        return `${dd / 3600 | 0}° ${dd % 3600 / 60 | 0}′ ${(dd % 60).toFixed(3)}″ ${dir}`;
+    }
+    static generate(lat, long) {
+        return `${DegreesMinutesSeconds.#generate_dms_str(lat, false)} ${DegreesMinutesSeconds.#generate_dms_str(long, true)}`;
+    }
+}
+
+export class Maidenhead {
+    static regex = /^[A-Ra-r]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2}([A-Xa-x]{2}([0-9]{2})?)?)?)?)?)?)?$/;
+    static #charCode_A = 'A'.charCodeAt(0);
+    static #charCode_a = 'a'.charCodeAt(0);
+    static #alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWX';
+    static #alphabet_lower = Maidenhead.#alphabet_upper.toLowerCase();
+    static tidy(str) {
+        return str.substr(0, 2).toUpperCase() + str.substr(2).toLowerCase();
+
+    }
+    static convert(str) {
+        let long = -180, lat = -90;
+        long += 20 * (str.charCodeAt(0) - Maidenhead.#charCode_A);
+        lat += 10 * (str.charCodeAt(1) - Maidenhead.#charCode_A);
+        let dlong = 20, dlat = 10;
+        for (let i = 1; 2 * i < str.length; ++i) {
+            if (i % 2 == 1) {
+                dlong /= 10; dlat /= 10;
+                long += str[2 * i] * dlong;
+                lat += str[2 * i + 1] * dlat;
+            } else {
+                dlong /= 24; dlat /= 24;
+                long += (str.charCodeAt(2 * i) - Maidenhead.#charCode_a) * dlong;
+                lat += (str.charCodeAt(2 * i + 1) - Maidenhead.#charCode_a) * dlat;
+            }
+        }
+        return [lat + dlat / 2, long + dlong / 2];
+    }
+    static generate(lat, long) {
+        if (long >= 180) long = -180;
+        long += 180; lat += 90;
+        let square = [long / 20, lat / 10].map(x => Maidenhead.#alphabet_upper[x | 0]).join('');
+        long = long % 20 / 20; lat = lat % 10 / 10;
+        for (let i = 1; i < 5; ++i) {
+            if (i % 2 == 1) {
+                square += `${long * 10 | 0}${lat * 10 | 0}`
+                long = long * 10 % 1;
+                lat = lat * 10 % 1;
+            } else {
+                square += [long * 24, lat * 24].map(x => Maidenhead.#alphabet_lower[x | 0]).join('');
+                long = long * 24 % 1;
+                lat = lat * 24 % 1;
+            }
+        }
+        return square;
+    }
+}
+
+export class New {
+    static regex = null;
+    static tidy(str) { }
+    static convert(str) { }
+    static generate(lat, long) { }
 }
--- main.js
+++ main.js
@@ -1,48 +1,35 @@
 import {
-    DD_RE,
-    DMS_RE,
-    MAIDENHEAD_RE,
-    tidy_dd,
-    tidy_dms,
-    tidy_maidenhead,
-    convert_dd,
-    convert_dms,
-    convert_maidenhead,
-    generate_dd,
-    generate_dms,
-    generate_maidenhead
+    DecimalDegrees,
+    DegreesMinutesSeconds,
+    Maidenhead
 } from "./coords.js";
 
 var lat, long, last;
 
-function updateValue(el, value) {
-    if (el == last) return;
-    el.value = value;
-}
-
-function updateAll() {
-    updateValue(document.querySelector('#dd'), generate_dd(lat, long));
-    updateValue(document.querySelector('#dms'), generate_dms(lat, long));
-    updateValue(document.querySelector('#maidenhead'), generate_maidenhead(lat, long));
-}
-
-function addInput(id, tidy, re, convert) {
-    document.querySelector(id).addEventListener('change', (ev) => {
-        ev.target.value = tidy(ev.target.value);
+function addInput(id, obj) {
+    const element = document.querySelector(id);
+    element.addEventListener('change', (ev) => {
+        ev.target.value = obj.tidy(ev.target.value);
     });
 
-    document.querySelector(id).addEventListener('input', (ev) => {
-        const value = tidy(ev.target.value);
-        const valid = re.test(value);
+    element.addEventListener('input', (ev) => {
+        const value = obj.tidy(ev.target.value);
+        const valid = obj.regex.test(value);
         if (!valid) { ev.target.classList.add("invalid"); return; }
         ev.target.classList.remove("invalid");
 
-        [lat, long] = convert(value);
+        [lat, long] = obj.convert(value);
         last = ev.target;
-        updateAll();
+        document.dispatchEvent(new Event("coordsupdate"));
     });
+
+    document.addEventListener("coordsupdate", () => {
+        if (element == last) return;
+        element.value = obj.generate(lat, long);
+
+    })
 }
 
-addInput('#dd', tidy_dd, DD_RE, convert_dd);
-addInput('#dms', tidy_dms, DMS_RE, convert_dms);
-addInput('#maidenhead', tidy_maidenhead, MAIDENHEAD_RE, convert_maidenhead);
+addInput('#dd', DecimalDegrees);
+addInput('#dms', DegreesMinutesSeconds);
+addInput('#maidenhead', Maidenhead);
